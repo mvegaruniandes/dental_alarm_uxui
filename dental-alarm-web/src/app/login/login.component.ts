386ceeacb8ab4c67f6cf40359398af7a12e31b4c
migue_vega <m.vegar@uniandes.edu.co>
2024-03-11 23:56:27 -0500

    Se carga implementación y ajustes finales

--- dental-alarm-web/src/app/login/login.component.ts
+++ dental-alarm-web/src/app/login/login.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
+
+  redirectAppointments(): void {
+    this.router.navigate(['/appointments']);
+  }
 }
