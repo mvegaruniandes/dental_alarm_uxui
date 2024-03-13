b34d87fe0d4600511d8af6c36f12c988211373fe
fabio.castagnola <castagnola92@gmail.com>
2024-03-12 20:58:26 -0500

    Firts commit app

---
+++ dental-alarm-app/src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  email: string = '';
+  password: string = '';
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+  async home() {
+  }
+}
